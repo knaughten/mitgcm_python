947a04ebc2d0f4ea054e6274dc268e2a52442d01
Kaitlin Naughten <kaight@bas.ac.uk>
2018-05-10 10:49:34 +0100

    Fixed plotting issue

--- plots.py
+++ plots.py
@@ -1,8 +1,10 @@
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 def quick_plot (grid, var):
 
-    plt.clf()
-    plt.contourf(grid.lon_2d, grid.lat_2d, var, 50);
-    plt.colorbar()
-    plt.show()
+    fig, ax = plt.subplots()
+    img = ax.contourf(grid.lon_2d, grid.lat_2d, var, 50);
+    plt.colorbar(img)
+    fig.show()
