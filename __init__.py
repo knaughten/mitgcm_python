97d30437cc7b8535d0dab899e76555fe849261cb
Kaitlin Naughten <kaight@bas.ac.uk>
2018-08-23 12:59:25 +0100

    Typo fix

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # Import commonly used functions for the interpreter
 from grid import Grid
-from file_io import netcdf_read
+from file_io import read_netcdf
 from utils import mask_3d, mask_land, mask_land_ice, mask_except_ice
 from plot_1d import read_plot_timeseries
 from plot_latlon import latlon_plot, read_plot_latlon
