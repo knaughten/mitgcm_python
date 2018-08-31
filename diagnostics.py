fe8049a615dfc902f019256243ea9bd43723edc3
Kaitlin Naughten <kaight@bas.ac.uk>
2018-08-31 15:41:26 +0100

    In-situ freezing point

--- diagnostics.py
+++ diagnostics.py
@@ -10,14 +10,84 @@ from utils import z_to_xyz, add_time_dim, xy_to_xyz
 from averaging import area_integral
 
 
+# Calculate the adiabatic temperature gradient exactly like MITgcm does. This originates from section 7 of "Algorithms for computation of fundamental properties of seawater", UNESCO technical papers in marine science 44, 1983.
+# Helper function for in_situ_temp.
+
+# Arguments:
+# temp: temperature (degC)
+# salt: salinity (psu)
+# press: pressure (dbar ~= absolute value of depth in m)
+# Note that temp, salt, and press can be of any dimension, as long as they all match.
+
+# Output: adiabatic temperature gradient in degC/dbar, same dimension as input arguments.
+
+def ad_temp_grad (temp, salt, press):
+
+    s_ref = 35.
+    a0 = 3.5803e-5
+    a1 = 8.5258e-6
+    a2 = -6.836e-8
+    a3 = 6.6228e-10
+    b0 = 1.8932e-6
+    b1 = -4.2393e-8
+    c0 = 1.8741e-8
+    c1 = -6.7795e-10
+    c2 = 8.733e-12
+    c3 = -5.4481e-14
+    d0 = -1.1351e-10
+    d1 = 2.7759e-12
+    e0 = -4.6206e-13
+    e1 = 1.8676e-14
+    e2 = -2.1687e-16
+
+    return a0 + a1*temp + a2*temp**2 + a3*temp**3 + (b0 + b1*temp)*(salt - s_ref) + (c0 + c1*temp + c2*temp**2 + c3*temp**3 + (d0 + d1*temp)*(salt - s_ref))*press + (e0 + e1*temp + e2*temp**2)*press**2
+
+
+# Calculate in-situ temperature from potential temperature, exactly like MITgcm does.
+
+# Arguments:
+# temp: potential temperature (degC)
+# salt: salinity (psu)
+# z: depth (m, sign doesn't matter)
+
+# Output: in-situ temperature (degC), same dimension as input arguments
+
+def in_situ_temp (temp, salt, z):
+
+    press_ref = 0.
+    sqrt_2 = np.sqrt(2)
+
+    # Step 1
+    dpress = np.abs(z) - press_ref
+    dtemp = dpress*ad_temp_grad(temp, salt, press_ref)
+    temp_new = temp + 0.5*dtemp
+    q = dtemp
+
+    # Step 2
+    dtemp = dpress*ad_temp_grad(temp_new, salt, press_ref + 0.5*dpress)
+    temp_new += (1 - 1/sqrt_2)*(dtemp - q)
+    q = (2 - sqrt_2)*dtemp + (-2 + 3/sqrt_2)*q
+
+    # Step 3
+    dtemp = dpress*ad_temp_grad(temp_new, salt, press_ref + 0.5*dpress)
+    temp_new += (1 + 1/sqrt_2)*(dtemp - q)
+    q = (2 + sqrt_2)*dtemp + (-2 - 3/sqrt_2)*q
+
+    # Step 4
+    dtemp = dpress*ad_temp_grad(temp_new, salt, press_ref + dpress)
+    temp_new += (dtemp - 2*q)/6
+
+    return temp_new
+
+
 # Calculate the in-situ freezing point (helper function for t_minus_tf)
 
 # Arguments:
-# temp, salt, z: arrays of any dimension (but all the same dimension) containing temperature (C), salinity (psu), and depth (m, sign doesn't matter).
+# salt, z: arrays of any dimension (but both the same dimension) containing salinity (psu) and depth (m, sign doesn't matter).
 
-# Output: array of the same dimension as temp, salt, and z, containing the in-situ freezing point.
+# Output: array of the same dimension as salt and z, containing the in-situ freezing point in degC.
 
-def tfreeze (temp, salt, z):
+def tfreeze (salt, z):
 
     a0 = -0.0575
     b = -7.61e-4
@@ -26,7 +96,7 @@ def tfreeze (temp, salt, z):
     return a0*salt + b*abs(z) + c0
 
 
-# Calculate the difference from the in-situ freezing point.
+# Calculate the temperature difference from the in-situ freezing point.
 
 # Arguments:
 # temp, salt: arrays of temperature and salinity. They can be 3D (depth x lat x lon) or 4D (time x depth x lat x lon), in which case you need time_dependent=True.
@@ -46,7 +116,7 @@ def t_minus_tf (temp, salt, grid, time_dependent=False):
         # 4D arrays
         z = add_time_dim(z, temp.shape[0])
 
-    return temp - tfreeze(temp, salt, z)
+    return in_situ_temp(temp, salt, z) - tfreeze(salt, z)
 
 
 # Calculate the total mass loss or area-averaged melt rate.
