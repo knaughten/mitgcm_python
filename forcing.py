c94b2f84886e9dc940cd561f8eadbed27ffbbe8b
Kaitlin Naughten <kaight@bas.ac.uk>
2018-11-06 14:18:38 +0000

    Tweaking polynya sizes

--- forcing.py
+++ forcing.py
@@ -362,11 +362,11 @@ def polynya_mask (grid_path, polynya, mask_file, prec=64):
         lat0 = -65.
         rlon = 8.
         rlat = 2.
-    elif polynya == 'near_shelf':  # Area 2.1 x 10^5 km^2
+    elif polynya == 'near_shelf':  # Area 2.6 x 10^5 km^2
         lon0 = -30.
         lat0 = -70.
         rlon = 9.
-        rlat = 2.25
+        rlat = 2.2
     elif polynya == 'maud_rise_big':  # Area 6.2 x 10^5 km^2
         lon0 = 0.
         lat0 = -65.
@@ -388,7 +388,7 @@ def polynya_mask (grid_path, polynya, mask_file, prec=64):
     # Select the polynya region
     index = (grid.lon_2d - lon0)**2/rlon**2 + (grid.lat_2d - lat0)**2/rlat**2 <= 1
     mask[index] = 1
-    
+
     # Print the area of the polynya
     print 'Polynya area is ' + str(np.sum(grid.dA[index])*1e-6) + ' km^2'
     # Plot the mask
