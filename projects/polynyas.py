12f063356cd3c30255fc43f0bc129d5dcb8442c4
Kaitlin Naughten <kaight@bas.ac.uk>
2018-12-05 13:07:36 +0000

    Polynya timeseries

--- projects/polynyas.py
+++ projects/polynyas.py
@@ -2,7 +2,14 @@
 # Weddell Sea polynya project
 ##################################################################
 
+import sys
+
 from ..postprocess import precompute_timeseries
+from ..utils import real_dir
+from ..grid import Grid
+from ..plot_1d import timeseries_multi_plot
+from ..file_io import netcdf_time, read_netcdf
+from ..constants import deg_string
 
 
 # Get longitude and latitude at the centre of the polynya
@@ -34,19 +41,86 @@ def precompute_polynya_timeseries (mit_file, timeseries_file, polynya=None):
     precompute_timeseries(mit_file, timeseries_file, timeseries_types=timeseries_types, lon0=lon0, lat0=lat0)
 
 
+# Make a bunch of tiled/combined plots showing all polynya simulations at once.
+def prelim_plots (base_dir='./', fig_dir='./'):
 
-# Function to precompute timeseries
-#   Convective area (MLD > 2000 m)
-#   Net ismr of FRIS
-#   Net ismr of E Weddell ice shelves
-#   Weddell Gyre transport
-#   T & S depth-averaged through centre of polynya
-#   T & S volume-averaged in FRIS cavity
+    base_dir = real_dir(base_dir)
+    fig_dir = real_dir(fig_dir)
+
+    # File paths
+    output_dir = ['polynya_baseline/', 'polynya_maud_rise/', 'polynya_near_shelf/', 'polynya_maud_rise_big/', 'polynya_maud_rise_small/', 'polynya_maud_rise_5y/']
+    grid_dir = output_dir[0] + 'grid/'
+    timeseries_file = 'timeseries_polynya.nc'
+    avg_file = '1979_2016_avg.nc'
+    # Titles etc. for plotting
+    expt_names = ['Baseline', 'Maud Rise', 'Near Shelf', 'Maud Rise Big', 'Maud Rise Small', 'Maud Rise 5y']
+    expt_colours = ['black', 'blue', 'green', 'red', 'cyan', 'magenta']
+    num_expts = len(output_dir)
+
+    print 'Building grid'
+    grid = Grid(base_dir+grid_dir)
+
+    # Inner function to plot timeseries on the same axes, plus potentially a difference plot and/or a percent difference plot.
+    def plot_polynya_timeseries (var_name, title, units, use_baseline=True, diff=None, percent_diff=None):
+        
+        if use_baseline:
+            i0 = 0
+        else:
+            i0 = 1
+        if diff is None:
+            diff = use_baseline
+        if percent_diff is None:
+            percent_diff = diff
+        if percent_diff and not diff:
+            print "Error (plot_polynya_timeseries): can't make percent difference plot without a difference plot"
+            sys.exit()
+
+        # Read data
+        data = []
+        for i in range(i0, num_expts):
+            file_path = base_dir + output_dir[i] + timeseries_file
+            if i==i0:
+                # Read the time axis
+                time = netcdf_time(file_path)
+            # Read the variable
+            data_tmp = read_netcdf(file_path, var_name)
+            # Parcel into array
+            data.append(data_tmp)
+            
+        # Make the plot
+        timeseries_multi_plot(time, data, expt_names[i0:], expt_colours[i0:], title=title, units=units, fig_name=fig_dir+var_name+'.png')
+        
+        if diff:
+            # Also make a difference plot
+            # Calculate the difference
+            data_diff = []
+            for i in range(1, num_expts):
+                data_diff_tmp = trim_and_diff(time, time, data[0], data[i])
+                data_diff.append(data_diff_tmp)
+            # Make the plot
+            timeseries_multi_plot(time, data_diff, expt_names[1:], expt_colours[1:], title=title+' anomaly', units=units, fig_name=fig_dir+var_name+'_diff.png')
+
+            if percent_diff:
+                # Also make a percent difference plot
+                data_diff_percent = []
+                for i in range(1, num_expts):
+                    data_diff_percent.append(data_diff[i]/data[0]*100)
+                timeseries_multi_plot(time, data_diff_percent, expt_names[1:], expt_colours[1:], title=title+' % anomaly', fig_name=fig_dir+var_name+'_percent_diff.png')
+
+    # end inner function
+
+    # Now make the timeseries plots
+    plot_polynya_timeseries('conv_area', 'Convective area', r'million km$^2$')
+    plot_polynya_timeseries('fris_ismr', 'FRIS basal mass loss', 'Gt/y')
+    plot_polynya_timeseries('ewed_ismr', 'EWIS basal mass loss', 'Gt/y')
+    plot_polynya_timeseries('wed_gyre_trans', 'Weddell Gyre transport', 'Sv')
+    plot_polynya_timeseries('fris_temp', 'FRIS cavity temperature', deg_string+'C', percent_diff=False)
+    plot_polynya_timeseries('fris_salt', 'FRIS cavity salinity', 'psu', percent_diff=False)
+    plot_polynya_timeseries('temp_polynya', 'Temperature in polynya', deg_string+'C', use_baseline=False)
+    plot_polynya_timeseries('salt_polynya', 'Salinity in polynya', 'psu', use_baseline=False)
 
 
-# Function to preliminarily analyse everything
-#   All timeseries on same axes
-#   Difference timeseries (all except polynya T & S) on same axes
+
 #   Lat-lon plots, averaged over entire period (all except 5-year)
 #     Sea ice area (absolute)
 #     Baseline absolute and others differenced, zoomed in and out:
