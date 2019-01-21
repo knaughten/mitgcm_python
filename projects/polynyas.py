d342df9ec834adfa7d78b1526daa0d8daadc93b7
Kaitlin Naughten <kaight@bas.ac.uk>
2019-01-21 15:22:34 +0000

    Add change points for polynya ismr figure

--- projects/polynyas.py
+++ projects/polynyas.py
@@ -611,7 +611,7 @@ def baseline_panels (base_dir='./', fig_dir='./'):
     for i in range(len(data)):
         # Leave the top left plot empty for title
         ax = plt.subplot(gs[(i+1)/3, (i+1)%3])
-        img = latlon_plot(data[i], grid, ax=ax, ctype=ctype[i], vmin=vmin[i], vmax=vmax[i], extend=extend[i], zoom_fris=True, title=title[i])
+        img = latlon_plot(data[i], grid, ax=ax, ctype=ctype[i], vmin=vmin[i], vmax=vmax[i], extend=extend[i], zoom_fris=True, title=title[i], change_points=[0.5, 1.5, 4])
         if ctype[i] == 'vel':
             # Overlay velocity vectors
             overlay_vectors(ax, u, v, grid, chunk=7, scale=0.6)
