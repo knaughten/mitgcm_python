Fixed masking bug with pload_anomaly

ics_obcs.py
@@ -181,17 +181,22 @@ def calc_load_anomaly (grid, out_file, option='constant', ini_temp_file=None, in
         salt[closed] = constant_s
     elif option == 'nearest':
         # Select the layer immediately below the ice shelves and tile to make it 3D
-        temp_top = xy_to_xyz(select_top(np.ma.masked_where(closed, temp)), grid)
-        salt_top = xy_to_xyz(select_top(np.ma.masked_where(closed, salt)), grid)
+        temp_top = xy_to_xyz(select_top(np.ma.masked_where(closed, temp), return_masked=False), grid)
+        salt_top = xy_to_xyz(select_top(np.ma.masked_where(closed, salt), return_masked=False), grid)
         # Fill the mask with these values
         temp[closed] = temp_top[closed]
-        salt[closed] = salt_top[closed]
+        salt[closed] = salt_top[closed]    
     elif option == 'precomputed':
-        # Make sure there are no missing values
         for data in [temp, salt]:
-            if (isinstance(data, np.ma.MaskedArray) and (closed!=data.mask).any()) or (data[~closed]==0).any():
+            # Make sure there are no missing values
+            if (data[~closed]==0).any():
                 print 'Error (calc_load_anomaly): you selected the precomputed option, but there are appear to be missing values in the land mask.'
                 sys.exit()
+            # Make sure it's not a masked array as this will break the rms
+            if isinstance(data, np.ma.MaskedArray):
+                # Fill the mask with zeros
+                data[data.mask] = 0
+                data = data.data
     else:
         print 'Error (calc_load_anomaly): invalid option ' + option
         sys.exit()
@@ -240,10 +245,7 @@ def calc_load_anomaly (grid, out_file, option='constant', ini_temp_file=None, in
     pload_below = select_top(np.ma.masked_where(closed, pload_edges))
     hfac_below = select_top(np.ma.masked_where(closed, hfac))
     # Now we can interpolate to the ice base
-    pload_masked = pload_above + (1-hfac_below)*(pload_below - pload_above)
-    # Fill the mask with zeros
-    pload = pload_masked.data
-    pload[pload_masked.mask] = 0
+    pload = pload_above + (1-hfac_below)*(pload_below - pload_above)
 
     # Write to file
     write_binary(pload, out_file, prec=prec)

utils.py
@@ -34,7 +34,7 @@ def xy_to_xyz (data, grid):
     else:
         nz = grid.nz
 
-    return np.tile(np.copy(data), (nz, 1, 1))
+    return np.tile(data, (nz, 1, 1))
 
 
 # Tile a 1D depth array in lat and lon so it is 3D (depth x lat x lon).
@@ -60,7 +60,7 @@ def add_time_dim (data, num_time):
 
 
 # Helper function for select_top and select_bottom
-def select_level (option, data, masked=True, grid=None, gtype='t', time_dependent=False):
+def select_level (option, data, masked=True, grid=None, gtype='t', time_dependent=False, return_masked=None):
 
     if not masked:
         if grid is None:
@@ -69,6 +69,8 @@ def select_level (option, data, masked=True, grid=None, gtype='t', time_dependen
         data_masked = mask_3d(np.copy(data), grid, gtype=gtype, time_dependent=time_dependent)
     else:
         data_masked = data
+    if return_masked is None:
+        return_masked = masked
 
     # Figure out the dimensions of the data when the vertical dimension is removed
     collapsed_shape = data_masked.shape[:-3] + data_masked.shape[-2:]
@@ -93,7 +95,7 @@ def select_level (option, data, masked=True, grid=None, gtype='t', time_dependen
     # Anything still NaN is land; mask it out
     data_lev = np.ma.masked_where(np.isnan(data_lev), data_lev)
 
-    if not masked:
+    if not return_masked:
         # Fill the mask with zeros
         data_lev[data_lev.mask] = 0
         data_lev = data_lev.data
@@ -102,15 +104,15 @@ def select_level (option, data, masked=True, grid=None, gtype='t', time_dependen
 
 
 # Select the top layer from the given array of data. This is useful to see conditions immediately beneath ice shelves.
-# If masked=True (default), the input array is already masked with hfac (see mask_3d below). If masked=False, you need to supply the keyword arguments grid, gtype, and time_dependent (as in mask_3d).
+# If masked=True (default), the input array is already masked with hfac (see mask_3d below). If masked=False, you need to supply the keyword arguments grid, gtype, and time_dependent (as in mask_3d). You can also control whether or not the output array is a masked array using return_masked (default the same value as masked).
 # The only assumption about the input array dimensions is that the third last dimension is the vertical dimension. So it can be depth x lat x lon, or time x depth x lat x lon, or even something like experiment x time x depth x lat x lon.
-def select_top (data, masked=True, grid=None, gtype='t', time_dependent=False):
-    return select_level('top', data, masked=masked, grid=grid, gtype=gtype, time_dependent=time_dependent)
+def select_top (data, masked=True, grid=None, gtype='t', time_dependent=False, return_masked=None):
+    return select_level('top', data, masked=masked, grid=grid, gtype=gtype, time_dependent=time_dependent, return_masked=return_masked)
 
     
 # Select the bottom layer from the given array of data. See select_top for more documentation.
-def select_bottom (data, masked=True, grid=None, gtype='t', time_dependent=False):
-    return select_level('bottom', data, masked=masked, grid=grid, gtype=gtype, time_dependent=time_dependent)
+def select_bottom (data, masked=True, grid=None, gtype='t', time_dependent=False, return_masked=None):
+    return select_level('bottom', data, masked=masked, grid=grid, gtype=gtype, time_dependent=time_dependent, return_masked=return_masked)
 
 
 # Helper function for masking functions below