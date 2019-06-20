cfe0d9d7738ee5ed1af5bc32e84cbe284a8fc668
Kaitlin Naughten <kaight@bas.ac.uk>
2019-06-20 14:57:17 +0100

    Transport calculations across boundaries

--- projects/tuning.py
+++ projects/tuning.py
@@ -363,6 +363,38 @@ def calc_volume_components (file_path, nc_out):
     for i in range(len(vars)):
         ncfile.add_variable(var_names[i], vars[i], 't', long_name=titles[i], units='m^3')
     ncfile.close()
+
+
+def bdry_transports (obcs_e_u, obcs_n_v, file_path, nc_out, sponge=8):
+
+    grid = Grid(file_path)
+
+    # Calculate areas of faces on boundaries
+    area_e = add_time_dim((xy_to_xyz(grid.dy_w, grid)*z_to_xyz(grid.dz, grid)*grid.hfac)[:,:,-1], num_time)
+    area_n = add_time_dim((xy_to_xyz(grid.dx_s, grid)*z_to_xyz(grid.dz, grid)*grid.hfac)[:,-1,:], num_time)
+
+    ncfile = NCfile(nc_out, grid, 't')
+    ncfile.add_time(np.arange(12)+1, units='months')
+
+    def calc_save_transport (vnorm, direction, title):
+        if direction == 'E':
+            area = area_e
+        elif direction == 'N':
+            area = area_n
+        else:
+            print 'Error (calc_save_transport): invalid direction ' + direction
+            sys.exit()
+        trans = np.sum(vnorm*area, axis=(1,2))*1e-6
+        ncfile.add_variable(title, trans, 't', units='Sv')
+
+    vnorms = [-1*read_binary(obcs_e_u, [grid.nx, grid.ny, grid.nz], 'yzt'), -1*read_binary(obcs_n_v, [grid.nx, grid.ny, grid.nz], 'xzt'), -1*read_netcdf(file_path, 'UVEL')[:,:,-1], -1*read_netcdf(file_path, 'VVEL')[:,-1,:], -1*read_netcdf(file_path, 'UVEL')[:,:,-1*sponge], -1*read_netcdf(file_path, 'VVEL')[:,-1*sponge,:]]
+    directions = ['E', 'N', 'E', 'N', 'E', 'N']
+    titles = ['obcs_e', 'obcs_n', 'outer_e', 'outer_n', 'inner_e', 'inner_n']
+    for i in range(len(titles)):
+        calc_save_transport(vnorms[i], directions[i], titles[i])
+
+    ncfile.close()
+    
     
 
     
