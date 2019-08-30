d835a8b2d766261ccc9d116d89deb394820d72fe
Kaitlin Naughten <kaight@bas.ac.uk>
2019-08-30 15:50:57 +0100

    Ua accent in title

--- projects/fris_coupled.py
+++ projects/fris_coupled.py
@@ -17,7 +17,9 @@ from ..plot_utils.latlon import cell_boundaries
 from ..plot_utils.labels import latlon_axes
 from ..plot_utils.windows import finished_plot
 
-def plot_domain (ua_mesh_file='ua_run/NewMeshFile.mat', output_dir='output/', fig_name=None):
+
+# Make a plot of the overlapping MITgcm grid and Ua mesh, at the beginning of the simulation.
+def plot_domain_mesh (ua_mesh_file='ua_run/NewMeshFile.mat', output_dir='output/', fig_name=None):
 
     output_dir = real_dir(output_dir)
 
@@ -48,5 +50,5 @@ def plot_domain (ua_mesh_file='ua_run/NewMeshFile.mat', output_dir='output/', fi
     # Turn off box
     ax.axis('off')
     # Title
-    plt.title('Initial MITgcm grid (blue) and Ua mesh (red)', fontsize=24)
+    plt.title('Initial MITgcm grid (blue) and $\'{U}$a mesh (red)', fontsize=24)
     finished_plot(fig, fig_name=fig_name)
