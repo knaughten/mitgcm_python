f932fd8bfff1bb830f549884c4b8cf95de9b649e
Kaitlin Naughten <kaight@bas.ac.uk>
2019-11-01 16:00:34 +0000

    WOA PICO forcing

--- projects/pico_intercomparison.py
+++ projects/pico_intercomparison.py
@@ -12,6 +12,7 @@ from ..plot_ua import read_ua_mesh
 from ..utils import real_dir, apply_mask, select_bottom
 from ..calculus import area_average
 from ..plot_utils.labels import round_to_decimals
+from ..grid import WOAGrid
 
 # Given the Moholdt basal melt rate data, extend into the mask until the entire Ua domain is covered.
 def extend_moholdt_data (old_file, new_file, ua_mesh_file):
@@ -48,32 +49,36 @@ def extend_moholdt_data (old_file, new_file, ua_mesh_file):
 # Calculate the annual mean and monthly climatology of WOA 2018's bottom temperature and salinity on the continental shelf in front of FRIS, as inputs to PICO. The results (24 values) will be printed on screen and also written to an ASCII file.
 def woa18_pico_input (woa_dir, out_file):
 
-    
+    file_head = 'woa18_decav_'
+    file_tail = '_04.nc'
+    var_names = ['t', 's']
+    var_name_tail = '_an'
+    woa_dir = real_dir(woa_dir)
 
-    
-
-    var_names = ['THETA', 'SALT']
-    file_tail = '_climatology.nc'
-    bsose_dir = real_dir(bsose_dir)
-
-    print 'Building B-SOSE grid'
-    grid = SOSEGrid(bsose_dir+'grid.nc')
+    print 'Building WOA grid'
+    # Use the January temperature file
+    grid = WOAGrid(woa_dir + file_head + var_names[0] + '01' + file_tail)
 
     f = open(out_file, 'w')
-    for i in range(2):
-        print 'Processing ' + var_names[i]
+    # Loop over variables
+    for n in range(2):
+        print 'Processing ' + var_names[n]
         f.write(var_names[i]+'\n')
-        # Read the data for all months
-        data = grid.read_field(bsose_dir+var_names[i]+file_tail, 'xyzt', var_name=var_names[i])        
-        # Select the bottom layer
-        data = select_bottom(data, masked=False, grid=grid, time_dependent=True)
-        # Mask out everything except the SWS continental shelf
-        data = apply_mask(data, np.invert(grid.sws_shelf_mask), time_dependent=True)
-        # Area-average
-        data = area_average(data, grid, time_dependent=True)
+        # Loop over months
         for t in range(12):
-            data_val = round_to_decimals(data[t], 2)
+            # Construct the filename
+            file_path = woa_dir + file_head + var_names[n] + str(t+1).zfill(2) + file_tail
+            # Read the data
+            data = read_netcdf(file_path, var_names[n]+var_name_tail)
+            # Select the bottom layer
+            data = select_bottom(data)
+            # Mask out everything except the SWS continental s helf
+            data = apply_mask(data, np.invert(grid.sws_shelf_mask))
+            # Area-average
+            data = area_average(data, grid)
+            # Format as a nice string
+            data_val = round_to_decimals(data, 2)
             print data_val
-            f.write(data_val+'\n')
+            f.write(data+val+'\n')
     f.close()
     print 'Data saved in ' + out_file
