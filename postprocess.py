915813dab827acfaf35753c02dceb58a05856ed2
Kaitlin Naughten <kaight@bas.ac.uk>
2020-01-23 14:02:42 +0000

    Typo fix

--- postprocess.py
+++ postprocess.py
@@ -990,7 +990,7 @@ def precompute_hovmoller (mit_file, hovmoller_file, loc=['PIB', 'Dot'], var=['te
             title = 'Salinity'
             units = 'psu'
         # Read data and mask land/ice shelves
-        data_full = mask_3d(read_netcdf(file_path, var_name), grid, time_dependent=True)
+        data_full = mask_3d(read_netcdf(mit_file, var_name), grid, time_dependent=True)
         for l in loc:
             print 'Processing ' + l            
             if l == 'PIB':
