f994e296ca087fc5f6427041fecbff3274c357e8
Kaitlin Naughten <kaight@bas.ac.uk>
2020-02-05 11:19:07 +0000

    Bug fix for monthly climatology

--- projects/pace.py
+++ projects/pace.py
@@ -3,6 +3,7 @@
 ##################################################################
 
 import numpy as np
+from itertools import compress
 
 from ..grid import ERA5Grid, PACEGrid
 from ..file_io import read_binary, write_binary
@@ -14,8 +15,9 @@ def calc_climatologies (era5_dir, pace_dir, out_dir):
     var_pace = ['TREFHT', 'QBOT', 'PSL', 'UBOT', 'VBOT', 'PRECT', 'FLDS', 'FSDS']
     file_head_era5 = 'ERA5_'
     file_head_pace = 'PACE_ens'
-    num_vars = len(var_era5)
     days_per_year = 365
+    months_per_year = 12
+    per_day = 4  # ERA5 records per day
     # Day of the year that's 29 Feb (0-indexed)
     leap_day = 31+28
     # Climatology over the years that both products have data (not counting the RCP8.5 extension)
@@ -24,6 +26,15 @@ def calc_climatologies (era5_dir, pace_dir, out_dir):
     num_years = end_year-start_year+1
     # Number of PACE ensemble members
     num_ens = 20
+
+    monthly = [var in ['FLDS', 'FSDS'] for var in var_pace]
+    var_era5_monthly = list(compress(var_era5, monthly))
+    var_era5_daily = list(compress(var_era5, np.invert(monthly)))
+    var_pace_monthly = list(compress(var_pace, monthly))
+    var_pace_daily = list(compress(var_pace, np.invert(monthly)))
+    num_vars = len(var_era5)
+    num_vars_monthly = len(var_era5_monthly)
+    num_vars_daily = len(var_era5_daily)    
 
     era5_grid = ERA5Grid()
     pace_grid = PACEGrid()
@@ -42,50 +53,76 @@ def calc_climatologies (era5_dir, pace_dir, out_dir):
     i_bins[i_bins==pace_grid.nx] = 0
     j_bins = np.digitize(era5_grid.lat, pace_lat_bins)
 
-    # Loop over variables
-    era5_clim = np.empty([num_vars, days_per_year, era5_grid.ny, era5_grid.nx])
-    print 'Processing ERA5'
-    for n in range(num_vars):
-        print 'Processing ' + var_pace[n]
-        
+    # Inner function to make climatology of an ERA5 variable
+    def era5_process_var (var_name_pace, var_name_era5, monthly):
+        print 'Processing ' + var_name_pace
+        if monthly:
+            per_year = months_per_year
+        else:
+            per_year = days_per_year        
         # Accumulate data over each year
-        data_accum = np.zeros([days_per_year, era5_grid.ny, era5_grid.nx])
+        data_accum = np.zeros([per_year, era5_grid.ny, era5_grid.nx])
         for year in range(start_year, end_year+1):
-            file_path = real_dir(era5_dir) + file_head_era5 + var_era5[n] + '_' + str(year)
+            file_path = real_dir(era5_dir) + file_head_era5 + var_name_era5 + '_' + str(year)
             data = read_binary(file_path, [era5_grid.nx, era5_grid.ny], 'xyt')
             # Average over each day
-            data = np.mean(np.reshape(data, (4, data.shape[0]/4, era5_grid.ny, era5_grid.nx)), axis=0)
-            if data.shape[0] == days_per_year+1:
+            data = np.mean(np.reshape(data, (per_day, data.shape[0]/per_day, era5_grid.ny, era5_grid.nx)), axis=0)
+            if monthly:
+                # Monthly averages
+                data_monthly = np.empty(data_accum.shape)
+                for month in range(months_per_year):
+                    nt = days_per_month(month+1, year)*per_day
+                    data_monthly[month,:] = np.mean(data[t:t+nt,:], axis=0)
+                    t += nt
+                data = data_monthly
+            elif data.shape[0] == days_per_year+1:
                 # Remove leap day
                 data = np.concatenate((data[:leap_day,:], data[leap_day+1:,:]), axis=0)
             data_accum += data
         # Convert from integral to average
-        data_clim = data_accum/num_years
-        era5_clim[n,:] = data_clim
+        return data_accum/num_years
+
+    # Loop over daily and monthly variables
+    print 'Processing ERA5'
+    era5_clim_daily = np.empty([num_vars_daily, days_per_year, era5_grid.ny, era5_grid.nx])
+    for n in range(num_vars_daily):
+        era5_process_var(var_pace_daily[n], var_era5_daily[n], False)
+    era5_clim_monthly = np.empty([num_vars_monthly, months_per_year, era5_grid.ny, era5_grid.nx])
+    for n in range(num_vars_monthly):
+        era5_process_var(var_pace_monthly[n], var_era5_monthly[n], True)
 
     # Now do all the binning at once to save memory
-    era5_clim_regrid = np.zeros([num_vars, days_per_year, pace_grid.ny, pace_grid.nx])
+    era5_clim_regrid_daily = np.zeros([num_vars_daily, days_per_year, pace_grid.ny, pace_grid.nx])
+    era5_clim_regrid_monthly = np.zeros([num_vars_monthly, months_per_year, pace_grid.ny, pace_grid.nx])
     print 'Regridding from ERA5 to PACE grid'
     for j in range(pace_grid.ny):
         for i in range(pace_grid.nx):
             if np.any(i_bins==i) and np.any(j_bins==j):
                 index = (i_bins==i)*(j_bins==j)
-                era5_clim_regrid[:,:,j,i] = np.mean(era5_clim[:,:,index], axis=-1)
+                era5_clim_regrid_daily[:,:,j,i] = np.mean(era5_clim_daily[:,:,index], axis=-1)
+                era5_clim_regrid_monthly[:,:,j,i] = np.mean(era5_clim_monthly[:,:,index], axis=-1)
     # Write each variable to binary
-    for n in range(num_vars):   
-        file_path = real_dir(out_dir) + 'ERA5_' + var_pace[n] + '_clim'
-        write_binary(era5_clim_regrid[n,:], file_path)
+    for n in range(num_vars_daily):   
+        file_path = real_dir(out_dir) + 'ERA5_' + var_pace_daily[n] + '_clim'
+        write_binary(era5_clim_regrid_daily[n,:], file_path)
+    for n in range(num_vars_monthly):   
+        file_path = real_dir(out_dir) + 'ERA5_' + var_pace_monthly[n] + '_clim'
+        write_binary(era5_clim_regrid_monthly[n,:], file_path)
 
     print 'Processing PACE'
     for n in range(num_vars):
         print 'Processing ' + var_pace[n]
+        if monthly[n]:
+            per_year = months_per_year
+        else:
+            per_year = days_per_year
         for ens in range(1, num_ens+1):
             if ens == 13:
                 continue
             ens_str = str(ens).zfill(2)
             print 'Processing PACE ensemble member ' + ens_str
             # As before, but simpler because no leap days and no need to regrid
-            data_accum = np.zeros([days_per_year, pace_grid.ny, pace_grid.nx])
+            data_accum = np.zeros([per_year, pace_grid.ny, pace_grid.nx])
             for year in range(start_year, end_year+1):
                 file_path = real_dir(pace_dir) + file_head_pace + ens_str + '_' + var_pace[n] + '_' + str(year)
                 data = read_binary(file_path, [pace_grid.nx, pace_grid.ny], 'xyt')
