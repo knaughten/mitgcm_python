fd91f4fe3dc2d0854e10871f01f7882529b47535
Kaitlin Naughten <kaight@bas.ac.uk>
2020-03-31 11:26:32 +0100

    GL plot tweaks

--- projects/threshold.py
+++ projects/threshold.py
@@ -353,7 +353,7 @@ def gl_plot (base_dir='./', fig_dir='./'):
     fig, ax = plt.subplots(figsize=(7,6))
     for n in range(len(xGL_all)):
         ax.plot(xGL_all[n], yGL_all[n], '-', color=colours[n], label=labels[n])
-    ax.legend(loc='upper center')
+    ax.legend(loc='upper left')
     ax.set_xticklabels([])
     ax.set_yticklabels([])
     ax.set_title('Final grounding line position')
