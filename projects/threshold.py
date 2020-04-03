7a17c731d61239f78d45a732de51df4cac51f639
Kaitlin Naughten <kaight@bas.ac.uk>
2020-04-03 13:03:45 +0100

    Typo fix

--- projects/threshold.py
+++ projects/threshold.py
@@ -471,7 +471,7 @@ def plot_inflow_zoom (base_dir='./', fig_dir='./'):
     [xmin, xmax, ymin, ymax] = [-50, -20, -77, -73]
     h0 = -1250
     chunk = 4
-    vmin_impose = [None, 33.6, something, None]
+    vmin_impose = [None, 33.6, None, None]
 
     grid = Grid(grid_path)
     base_dir = real_dir(base_dir)
