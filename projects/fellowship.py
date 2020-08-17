48549c44f155e73e65607bbeffd693936535fba6
Kaitlin Naughten <kaight@bas.ac.uk>
2020-08-17 16:56:27 +0100

    Schmidtko figure

--- projects/fellowship.py
+++ projects/fellowship.py
@@ -7,9 +7,11 @@ import matplotlib.pyplot as plt
 
 from ..file_io import read_netcdf, netcdf_time
 from ..timeseries import calc_annual_averages
-from ..utils import moving_average, polar_stereo, select_bottom
+from ..utils import moving_average, polar_stereo, select_bottom, bdry_from_hfac
 from ..constants import deg_string
 from ..plot_utils.windows import finished_plot, set_panels
+from ..plot_utils.colours import set_colours
+from ..interpolation import interp_reg_xy
 
 def extract_geomip_westerlies ():
 
@@ -63,46 +65,65 @@ def extract_geomip_westerlies ():
 
 
 # Plot Paul's SO bottom temps versus observations
-def bottom_temp_vs_obs (model_file='stateBtemp_avg.nc', obs_file='woa18_decav_t00_04.nc'):
+def bottom_temp_vs_obs (model_file='stateBtemp_avg.nc', model_grid='grid.glob.nc', obs_file='schmidtko_data.txt'):
+
+    # TODO
+    # Continent shaded in grey
 
     # Set spatial bounds (60S at opposite corners)
     corner_lon = np.array([-45, 135])
     corner_lat = np.array([-60, -60])
     corner_x, corner_y = polar_stereo(corner_lon, corner_lat)
-    [xmin, xmax, ymin, ymax] = [corner_x[0], corner_y[0], corner_x[1], corner_y[1]]
+    [xmin, xmax, ymin, ymax] = [corner_x[0], corner_x[1], corner_y[1], corner_y[0]]
     # Set colour bounds
-    vmin = -3
-    vmax = 3
+    vmin = -2
+    vmax = 1.75
     lev = np.linspace(vmin, vmax, num=30)
 
     # Read model data
     model_lon = read_netcdf(model_file, 'LONGITUDE')
     model_lat = read_netcdf(model_file, 'LATITUDE')
     model_temp = read_netcdf(model_file, 'BTEMP')
-    # Apply land mask (filled with zeros)
-    model_temp = np.ma.masked_where(model_temp==0, model_temp)
+    # Read other grid variables
+    hfac = read_netcdf(model_grid, 'HFacC')
+    z_edges = read_netcdf(model_grid, 'RF')
+    bathy = bdry_from_hfac('bathy', hfac, z_edges)
+    draft = bdry_from_hfac('draft', hfac, z_edges)
+    # Mask out land, ice shelves, deep ocean
+    mask = (bathy!=0)*(draft==0)*(bathy>-1500)
+    model_temp = np.ma.masked_where(np.invert(mask), model_temp)
     # Convert coordinates to polar stereographic
-    model_lon, model_lat = np.meshgrid(model_lon, model_lat)
     model_x, model_y = polar_stereo(model_lon, model_lat)
 
-    # Read WOA data
-    obs_lon = read_netcdf(obs_file, 'lon')
-    obs_lat = read_netcdf(obs_file, 'lat')
-    obs_temp_3d = np.squeeze(read_netcdf(obs_file, 't_an'))
-    # Extract bottom temperatures: deepest unmasked values
-    obs_temp = select_bottom(obs_temp_3d)
-    # Convert coordinates to polar stereographic
-    obs_lon, obs_lat = np.meshgrid(obs_lon, obs_lat)
-    obs_x, obs_y = polar_stereo(obs_lon, obs_lat)
+    # Read obs data
+    obs = np.loadtxt(obs_file, dtype=np.str)
+    obs_lon = obs[:,0].astype(float)
+    obs_lat = obs[:,1].astype(float)
+    obs_depth = obs[:,2].astype(float)
+    obs_temp = obs[:,3].astype(float)
+    # Interpolate to model grid
+    obs_temp = interp_reg_xy(obs_lon, obs_lat, obs_temp, model_lon, model_lat)
+    # Mask as before
+    obs_temp = np.ma.masked_where(np.invert(mask), obs_temp)
 
     # Plot
-    fig, gs, cax = set_panels('1x2C1')
-    ax = plt.subplot(gs[0,0])
-    img = ax.contourf(model_x, model_y, model_temp, lev)
-    ax.set_title('Existing model', fontsize=18)
-    ax = plt.subplot(gs[0,1])
-    img = ax.contourf(obs_x, obs_y, obs_temp, lev)
-    ax.set_title('Observations', fontsize=18)
-    cbar = plt.colorbar(img, cax=cax, extend='both', orientation='horizontal')
-    plt.suptitle('Bottom temperatures ('+deg_string+'C)', fontsize=24)
-    fig.show()
+    cmap = set_colours(model_temp, ctype='plusminus', vmin=vmin, vmax=vmax)[0]
+    data = [model_temp, obs_temp]
+    titles = ['a) Existing model', 'b) Observations']
+    fig, gs = set_panels('1x2C0', figsize=(8,4))
+    for n in range(2):
+        ax = plt.subplot(gs[0,n])
+        img = ax.contourf(model_x, model_y, data[n], lev, cmap=cmap, extend='both')
+        ax.set_title(titles[n], fontsize=16)
+        ax.axis('equal')
+        ax.set_xlim([xmin, xmax])
+        ax.set_ylim([ymin, ymax])
+        ax.set_xticks([])
+        ax.set_yticks([])
+    cax = fig.add_axes([0.01, 0.3, 0.02, 0.4])
+    cax.yaxis.set_label_position('left')
+    cax.yaxis.set_ticks_position('left')
+    cbar = plt.colorbar(img, cax=cax,ticks=np.arange(-2, 2, 1))
+    cax.tick_params(length=2)
+    plt.suptitle('Bottom temperatures ('+deg_string+'C)', fontsize=18)
+    finished_plot(fig, fig_name='bwtemp_compare.png')
