Fixed bug with depth-averaging

calculus.py
@@ -71,10 +71,14 @@ def prepare_dz_hfac (data, grid, gtype='t', time_dependent=False):
 def over_depth (option, data, grid, gtype='t', time_dependent=False):
 
     dz, hfac = prepare_dz_hfac(data, grid, gtype=gtype, time_dependent=time_dependent)
+    if isinstance(data, np.ma.MaskedArray):
+        mask = np.invert(data.mask).astype(float)
+    else:
+        mask = np.ones(data.shape)
     if option == 'average':
-        return np.sum(data*dz*hfac, axis=-3)/np.sum(dz*hfac, axis=-3)
+        return np.sum(data*dz*hfac*mask, axis=-3)/np.sum(dz*hfac*mask, axis=-3)
     elif option == 'integrate':
-        return np.sum(data*dz*hfac, axis=-3)
+        return np.sum(data*dz*hfac*mask, axis=-3)
     else:
         print 'Error (over_depth): invalid option ' + option
         sys.exit()

projects/threshold.py
@@ -18,7 +18,7 @@ import datetime
 
 from ..grid import Grid, choose_grid, UKESMGrid
 from ..file_io import read_netcdf, NCfile, netcdf_time, read_iceprod, read_binary, NCfile_basiclatlon
-from ..utils import real_dir, var_min_max, select_bottom, mask_3d, mask_except_ice, convert_ismr, add_time_dim, mask_land, xy_to_xyz, moving_average, mask_land_ice, fix_lon_range, split_longitude, polar_stereo, z_to_xyz
+from ..utils import real_dir, var_min_max, select_bottom, mask_3d, mask_except_ice, convert_ismr, add_time_dim, mask_land, xy_to_xyz, moving_average, mask_land_ice, fix_lon_range, split_longitude, polar_stereo, z_to_xyz, mask_2d_to_3d
 from ..plot_utils.windows import finished_plot, set_panels
 from ..plot_utils.latlon import shade_land_ice, prepare_vel, overlay_vectors
 from ..plot_utils.labels import latlon_axes, parse_date, slice_axes, depth_axis, round_to_decimals
@@ -2724,8 +2724,8 @@ def plot_density_panels (precompute_file, base_dir='./', fig_dir='./'):
     base_dir = real_dir(base_dir)
     fig_dir = real_dir(fig_dir)
 
-    # Read the time-averaged density precomputed in file and subtract 1000 for ease of colourbars
-    density_all = read_netcdf(precompute_file, 'potential_density')-1e3
+    # Read the time-averaged density precomputed in file
+    density_all = read_netcdf(precompute_file, 'potential_density')
     # Also read the initial grid
     grid = Grid(base_dir+grid_path)
 
@@ -2734,39 +2734,39 @@ def plot_density_panels (precompute_file, base_dir='./', fig_dir='./'):
     for n in range(num_periods):
         # Get the mask as shown by the data, not the grid (as the grounding line changes and the data will have the most permissive mask for the given time period)
         mask = np.invert(density_all[n,:].mask)
-        # Now restrict to the FRIS region
-        fris_mask = grid.restrict_mask(mask, 'fris_cavity')
+        # Now restrict to the FRIS region, without relying on the ice mask (don't want to mask any ice that's sometimes grounded)
+        fris_mask = np.ma.masked_where(xy_to_xyz(grid.get_open_ocean_mask(), grid), grid.restrict_mask(mask, 'fris'))
         fris_density = np.ma.masked_where(np.invert(fris_mask), density_all[n,:])
         fris_mean_tmp = volume_average(fris_density, grid)
         fris_mean.append(fris_mean_tmp)
-        density_all[n,:] -= fris_mean_tmp
+        #density_all[n,:] -= fris_mean_tmp
 
     # Average below 300m on shelf, over all depths in cavity, and mask deep ocean
     # Do this by masking the upper open ocean and the entire deep ocean, then vertically average what's left
-    upper_ocean = mask_2d_to_3d(grid.get_open_ocean_mask(), zmin=h_front)
-    deep_ocean = xy_to_xyz(grid.bathy <= h_shelf)
-    density_final = np.empty([num_periods, grid.ny, grid.nx])
+    upper_ocean = mask_2d_to_3d(grid.get_open_ocean_mask(), grid, zmin=h_front)
+    deep_ocean = xy_to_xyz(grid.bathy <= h_shelf, grid)
+    density_final = np.ma.empty([num_periods, grid.ny, grid.nx])
     for n in range(num_periods):
         density_tmp = np.ma.masked_where(upper_ocean, density_all[n,:])
-        density_tmp = np.ma.masked_where(deep_ocean, density_tmp)
-        density_final[n,:] = vertical_average(density_tmp, grid)
+        #density_tmp = np.ma.masked_where(deep_ocean, density_tmp)
+        density_final[n,:] = mask_land(vertical_average(density_tmp, grid), grid)
     # Overwrite this with manual bounds?
     vmin = np.amin(density_final)
     vmax = np.amax(density_final)
 
     # Plot 4 panels
-    fig, gs, cax = set_panels('2x2C1')
+    fig, gs, cax = set_panels('2x2C1', figsize=(6,7))
     for n in range(num_periods):
         ax = plt.subplot(gs[n/2, n%2])
         ax.axis('equal')
-        img = latlon_plot(density_final[n,:], grid, ax=ax, make_cbar=False, ctype='plusminus', vmin=vmin, vmax=vmax, zoom_fris=True, pster=True, title=titles[n])
-        plt.text(0.99, 0.01, 'FRIS mean = '+round_to_decimals(fris_mean[n],3), ha='right', va='bottom', transform=ax.transAxes)
+        img = latlon_plot(density_final[n,:], grid, ax=ax, make_cbar=False, vmin=vmin, vmax=vmax, zoom_fris=True, pster=True, title=titles[n])
+        plt.text(0.99, 0.01, 'FRIS mean = \n'+round_to_decimals(fris_mean[n],3), ha='right', va='bottom', transform=ax.transAxes)
         ax.set_xticks([])
         ax.set_yticks([])
     cbar = plt.colorbar(img, cax=cax, orientation='horizontal')
-    plt.suptitle(r'Density difference from FRIS mean (kg/m$^3$-1000), abrupt-4xCO2', fontsize=24)
+    plt.suptitle(r'Density difference from FRIS mean (kg/m$^3$)', fontsize=20)
     finished_plot(fig) #, fig_name=fig_dir+'density_panels.png', dpi=300)
-    
+
             
             
         