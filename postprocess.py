a9ed72f898c1c709f246dad580b5c45ccdefe13f
Kaitlin Naughten <Kaitlin Naughten>
2022-05-05 13:59:33 +0100

    Fixed missing import

--- postprocess.py
+++ postprocess.py
@@ -11,10 +11,10 @@ import netCDF4 as nc
 from .grid import Grid
 from .file_io import NCfile, netcdf_time, find_time_index, read_netcdf, read_iceprod
 from .timeseries import calc_timeseries, calc_special_timeseries, set_parameters
-from .utils import real_dir, days_per_month, str_is_int, mask_3d, mask_except_ice, mask_land, mask_land_ice, select_top, select_bottom, mask_outside_box, var_min_max, add_time_dim, apply_mask
+from .utils import real_dir, days_per_month, str_is_int, mask_3d, mask_except_ice, mask_land, mask_land_ice, select_top, select_bottom, mask_outside_box, var_min_max, add_time_dim, apply_mask, convert_ismr
 from .constants import deg_string, region_names
 from .calculus import area_average
-from .diagnostics import density, convert_ismr
+from .diagnostics import density
 from .interpolation import interp_grid
 
 
