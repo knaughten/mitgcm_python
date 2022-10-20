5060c6392e9164543cc5c6a5e72fb366a523e320
Kaitlin Naughten <Kaitlin Naughten>
2022-10-20 11:53:11 +0100

    Made connected_mask iterative not recursive

--- interpolation.py
+++ interpolation.py
@@ -700,68 +700,70 @@ def distance_weighted_nearest_neighbours (data, weights=None, num_neighbours=10,
 
 
 # Given a boolean mask of up to 3 dimensions (eg land mask, ice mask...) and the coordinates of a point in which that mask is True (eg [j,i]), return another mask which is True at all the points which are connected to the original point. For example, to isolate an individual ice shelf, or a subglacial lake.
-# This function is recursive so the usual dangers apply
-def connected_mask (point, mask, is_connected=None):
+def connected_mask (point0, mask):
 
-    dim = len(point)
+    dim = len(point0)
     if dim == 1:
-        [i] = point
+        [i] = point0
         [nx] = mask.shape
     elif dim == 2:
-        [j,i] = point
+        [j,i] = point0
         [ny,nx] = mask.shape
     elif dim == 3:
-        [k,j,i] = point
+        [k,j,i] = point0
         [nz,ny,nx] = mask.shape
     elif dim > 3:
         print('Error (connected_mask): not implemented for more than 3 dimensions. This array has '+str(dim)+' dimensions.')
         sys.exit()
-    if not mask[point]:
+    if not mask[tuple(point0)]:
         print('Error (connected_mask): the given mask is False at the given point.')
         sys.exit()
 
-    if is_connected is None:
-        # Initialise connected mask to all False
-        is_connected = np.zeros(mask.shape).astype(bool)
+    # Initialise connected mask to all False
+    connected = np.zeros(mask.shape).astype(bool)
 
-    # Get immediate neighbours which are True in the given mask
-    neighbours = []
-    # Inner function to streamline this
-    def check_neighbour (dimension, increment):
+    # Inner function to check if a given neighbour to the given point is valid
+    def check_neighbour (point, dimension, increment, neighbours):
         new_point = np.copy(point)
         new_point[dimension] = point[dimension] + increment
-        if mask[new_point]:
+        if mask[tuple(new_point)]:
             neighbours.append(new_point)
-    # x dimension
-    if i > 0:
-        check_neighbour(-1, -1)
-    if i+1 < nx:
-        check_neighbour(-1, 1)
-    # y dimension
-    if dim > 1:
-        if j > 0:
-            check_neighbour(-2, -1)
-        if j+1 < ny:
-            check_neighbour(-2, 1)
-    # z dimension
-    if dim > 2:
-        if k > 0:
-            check_neighbour(-3, -1)
-        if k+1 < nz:
-            check_neighbour(-3, 1)
+        return neighbours
 
-    # Now loop over the valid neighbours
-    for new_point in neighbours:
-        if is_connected[new_point]:
-            # This point was dealt with previously
-            continue
-        else:
-            # Update connected mask
-            is_connected[new_point] = True
-            # Recursive call to function to deal with all of this point's neighbours
-            is_connected = connected_mask(new_point, mask, is_connected=is_connected)
+    # Inner function to get valid immediate neighbours at any point (up to 2*dim)
+    def get_immediate_neighbours (point):
+        neighbours = []
+        if i > 0:
+            neighbours = check_neighbour(point, -1, -1, neighbours)
+        if i+1 < nx:
+            neighbours = check_neighbour(point, -1, 1, neighbours)
+        if dim > 1:
+            if j > 0:
+                neighbours = check_neighbour(point, -2, -1, neighbours)
+            if j+1 < ny:
+                neighbours = check_neighbour(point, -2, 1, neighbours)
+        if dim > 2:
+            if k > 0:
+                neighbours = check_neighbour(point, -3, -1, neighbours)
+            if k+1 < nz:
+                neighbours = check_neighbour(point, -3, 1, neighbours)
+        return neighbours
 
-    return is_connected
+    # Get started with the immediate neighbours of point0 to initialise a LIFO stack
+    stack = get_immediate_neighbours(point0)
+
+    while len(stack) > 0:
+        new_point = stack.pop()
+        # Update connected mask
+        connected[tuple(new_point)] = True
+        # Get its immediate neighbours
+        neighbours = get_immediate_neighbours(new_point)
+        # Loop over these neighbours and add to the stack if they haven't already been dealt with
+        for possible_point in neighbours:
+            if not connected[tuple(possible_point)]:
+                stack.append(possible_point)
+
+    return connected
     
     
     
